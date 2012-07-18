f8955718793a59281312c680b9bcd560bfb03e37
John-Gee <john.ettedgui@gmail.com>
2012-07-18 01:11:38 -0700

    initial version of vertical tabs

--- src/libraries/MoviesManager.js
+++ src/libraries/MoviesManager.js
@@ -97,3 +97,12 @@ $(function() {
 $(function() {
 	$( "#tabs" ).tabs();
 });
+
+var $items = $('#vtab>ul>li');
+$items.mouseover(function() {
+    $items.removeClass('selected');
+    $(this).addClass('selected');
+
+    var index = $items.index($(this));
+    $('#vtab>div').hide().eq(index).show();
+}).eq(1).mouseover();
